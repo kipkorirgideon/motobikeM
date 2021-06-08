b90c2f222415d4f9025968f490a7f18bc99ca485
kipkorirgideon <korirgideon29@gmail.com>
2021-06-08 16:24:14 +0300

    Creating commit to the django  app migrations

--- bikeApp/models.py
+++ bikeApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Motobike(models.Model):
+    number_plate = models.IntegerField()
+    bike_model = models.CharField(max_length=200)
+    warranty = models.IntegerField()
+    bike_owner = models.CharField(max_length = 100)
+
